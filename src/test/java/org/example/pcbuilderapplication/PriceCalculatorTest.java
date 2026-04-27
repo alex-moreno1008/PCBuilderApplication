f59646a945911c3c8a9adf4fa8299c7130f1182b
alex-moreno1008 <alejandro.mh1008@gmail.com>
2026-04-26 20:45:28 -0700

    implemented issue #3 but still need to improve on

---
+++ src/test/java/org/example/pcbuilderapplication/PriceCalculatorTest.java
@@ -0,0 +1,24 @@
+package org.example.pcbuilderapplication;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PriceCalculatorTest {
+
+    @Test
+    void calculateTotal_shouldReturnCorrectSum() {
+        double total = PriceCalculator.calculateTotal(200.0, 300.0, 100.0, 150.0);
+
+        assertEquals(750.0, total);
+    }
+
+    @Test
+    void calculateTotal_withZeroValues() {
+
+        double total = PriceCalculator.calculateTotal(0, 0, 0, 0);
+
+        assertEquals(0, total);
+
+    }
+
+}
