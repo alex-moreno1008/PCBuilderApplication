5e317f686efc83f1aa1cbd010a16fc0b7c9675ce
Andrew Butac <andrewbutac@gmail.com>
2026-05-04 21:58:36 -0700

    Added a way to load and display user's saved builds

--- src/main/java/org/example/pcbuilderapplication/controllers/SavedBuildController.java
+++ src/main/java/org/example/pcbuilderapplication/controllers/SavedBuildController.java
@@ -3,8 +3,26 @@ package org.example.pcbuilderapplication.controllers;
 import javafx.fxml.FXML;
 import org.example.pcbuilderapplication.SceneManager;
 import org.example.pcbuilderapplication.SceneType;
+import org.example.pcbuilderapplication.models.UserService;
+import java.util.List;
+import javafx.scene.control.ListView;
+import org.example.pcbuilderapplication.DatabaseManager;
 
 public class SavedBuildController {
+    @FXML private ListView<String> savedBuildsList;
+
+    @FXML
+    public void initialize() {
+        int userId = UserService.getInstance().getLoggedInUserId();
+        DatabaseManager db = new DatabaseManager();
+        List<String> builds = db.getSavedBuildSummaries(userId);
+
+        if (builds.isEmpty()) {
+            savedBuildsList.getItems().add("No saved builds yet.");
+        } else {
+            savedBuildsList.getItems().addAll(builds);
+        }
+    }
 
     @FXML
     private void goHome() {
